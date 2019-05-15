8f9eadc759a23654e4f709cdc6ca9a9f08ec5c47
warrior0986 <diegoguerrero0986@gmail.com>
2019-05-14 22:23:58 -0500

    First Commit

---
+++ api/controllers/StreamController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace api\controllers;
+
+use yii\rest\ActiveController;
+
+class StreamController extends ActiveController
+{
+    public $modelClass = 'api\models\Stream';
+}
---
+++ api/models/Stream.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace api\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "stream".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property int $user_id
+ *
+ * @property User $user
+ */
+class Stream extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'stream';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'user_id'], 'required'],
+            [['description'], 'string'],
+            [['user_id'], 'integer'],
+            [['title'], 'string', 'max' => 50],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'user_id' => 'User ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
