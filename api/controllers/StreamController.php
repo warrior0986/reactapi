4523ff67d3e22d5c01baa54316cf4cc4788fa34b
warrior0986 <diegoguerrero0986@gmail.com>
2019-05-15 22:03:58 -0500

    Fixing API to receive calls from ReactJS

--- api/controllers/StreamController.php
+++ api/controllers/StreamController.php
@@ -7,4 +7,29 @@ use yii\rest\ActiveController;
 class StreamController extends ActiveController
 {
     public $modelClass = 'api\models\Stream';
+
+    public function behaviors() //https://stackoverflow.com/questions/53807205/yii2-restful-api-reason-cors-header-access-control-allow-origin-missing
+    {
+        $behaviors = parent::behaviors();
+
+        $auth=$behaviors['authenticator'];
+
+        // remove authentication filter necessary because we need to 
+        // add CORS filter and it should be added after the CORS
+        unset($behaviors['authenticator']);
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => '\yii\filters\Cors',
+            'cors' => [
+                'Origin' => ['http://localhost:3000'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+            ],
+        ];
+
+        // re-add authentication filter
+        $behaviors['authenticator'] = $auth;
+        return $behaviors;
+    }
 }
--- api/models/Stream.php
+++ api/models/Stream.php
@@ -3,6 +3,7 @@
 namespace api\models;
 
 use Yii;
+use common\models\User;
 
 /**
  * This is the model class for table "stream".
